cf1306f0d2cf8c0f704206ed7364858e14974a32
dev-arvel-lr <dev_arvel@lrtechs.co.jp>
2025-09-29 15:50:02 +0800

    adding the hash on each url

--- src/pages/auth/Signup.tsx
+++ src/pages/auth/Signup.tsx
@@ -48,7 +48,7 @@ export default function Signup() {
 
   useEffect(() => {
     if (isAuthenticated) {
-      navigate("/");
+      navigate("/#/");
     }
   }, [isAuthenticated, navigate]);
 
@@ -171,7 +171,7 @@ export default function Signup() {
               <div className="text-muted-foreground flex justify-center gap-1 text-sm mt-3">
                 <p>Already a user?</p>
                 <a
-                  href="/login"
+                  href="/#/login"
                   className="text-primary dark:text-foreground font-medium hover:underline"
                 >
                   Login
